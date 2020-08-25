a66ecd04c7d8be64cfa9636f854e9d8839ff0379
Jagan1992 <Jagadesh043@gmail.com>
2020-08-25 16:11:11 +0530

    Added the condition for storing the data in redux only in dev enviroment

--- src/reducer/store.js
+++ src/reducer/store.js
@@ -4,7 +4,12 @@ import rootReducer from "./root-reducer";
 import { composeWithDevTools } from "redux-devtools-extension";
 import { persistStore } from "redux-persist"; //for maintaining the data in local storage using persist store.
 
-const middlewares = [logger];
+const middlewares = [];
+
+//it stores the data only in development.
+if (process.env.NODE_ENV === "development") {
+  middlewares.push(logger);
+}
 
 const composeEnhancers = composeWithDevTools({
   name: `Redux`,
